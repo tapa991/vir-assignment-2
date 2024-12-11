a26ae1412489ec28086cf281e77ef5cca0e65802
tapa991 <tapa@sheridancollege.ca>
2024-12-11 12:38:16 -0500

    Changed text

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`App running on http://localhost:${PORT}`);
+  console.log(`The app running on http://localhost:${PORT}`);
 });
